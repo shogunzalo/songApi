16b655436558d023bbc2f8c1e1bb1be4140cc3e7
dangry <g_rdo@live.cl>
2016-02-26 01:02:26 -0300

    Enabling signup

--- app/routes.js
+++ app/routes.js
@@ -41,16 +41,16 @@ module.exports = function(app, passport) {
     //     // render the page and pass in any flash data if it exists
     //     res.render('signup.ejs', { message: req.flash('signupMessage') });
     //});
-    //app.get('/signup', function(req, res) {
-    //    res.sendfile(viewRoute + '/signup.html');// load the index.ejs file
-    //});
-    //
-    //// process the signup form
-    //app.post('/signup', passport.authenticate('local-signup', {
-    //    successRedirect : '/admin', // redirect to the secure profile section
-    //    failureRedirect : '/signup', // redirect back to the signup page if there is an error
-    //    failureFlash : true // allow flash messages
-    //}));
+    app.get('/signup', function(req, res) {
+        res.sendfile(viewRoute + '/signup.html');// load the index.ejs file
+    });
+
+    // process the signup form
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect : '/admin', // redirect to the secure profile section
+        failureRedirect : '/signup', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
 
     // =====================================
     // PROFILE SECTION =====================
